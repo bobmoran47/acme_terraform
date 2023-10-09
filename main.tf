ddc55b18e0d81c878e025dff56d56c3e4a013708
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-08 21:42:56 -0400

    Update main.tf - add AWS Instances

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
  resource "aws_instance" "web" {
    ami           = data.aws_ami.app_ami.id
     instance_type = var.instance_type
+    count = 4
  
    tags = {
      Name = "ACME Company Terraform Presentation"
