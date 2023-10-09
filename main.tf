021b0989223afd7c7838018669ec04658c054663
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-09 16:01:52 -0400

    Security Groups Registry Module

--- main.tf
+++ main.tf
@@ -21,12 +21,19 @@ data "aws_vpc" "default" {
 resource "aws_instance" "acme" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.acme.id]
+  
+  vpc_security_group_ids = [module.acme_sg.security_group_id]
 
   tags = {
     Name = "ACME Terraform"
   }
 }
+
+module "acme_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+}
+vpc_id  = data.aws_vpc.default.id
 
 resource "aws_security_group" "acme" {
   name = "acme"
@@ -36,14 +43,15 @@ resource "aws_security_group" "acme" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "acme_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme.id
+  name    = "acme"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
+
 
 
 resource "aws_security_group_rule" "acme_https_in" {
