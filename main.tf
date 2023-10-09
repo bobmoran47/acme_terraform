b3e1aaa595c48197fc093ef2745c3c51a1d06e11
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-09 16:17:39 -0400

    Update Modules from Registry - Updated

--- main.tf
+++ main.tf
@@ -21,54 +21,21 @@ data "aws_vpc" "default" {
 resource "aws_instance" "acme" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  
   vpc_security_group_ids = [module.acme_sg.security_group_id]
 
   tags = {
-    Name = "ACME Terraform"
+    Name = "ACME Company Terraform Example "
   }
 }
 
 module "acme_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-}
-vpc_id  = data.aws_vpc.default.id
+  version = "4.13.0"
 
-resource "aws_security_group" "acme" {
-  name = "acme"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
-
+  vpc_id  = data.aws_vpc.default.id
   name    = "acme"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-
-
-resource "aws_security_group_rule" "acme_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme.id
-}
-
-
-resource "aws_security_group_rule" "acme_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme.id
 }
