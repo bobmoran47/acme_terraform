8d3b43e7cb8f01f416d855034673a4946dc4ee5a
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-08 22:08:52 -0400

    Update main.tf - return to original

--- main.tf
+++ main.tf
@@ -15,22 +15,13 @@ data "aws_ami" "app_ami" {
  }
   
  resource "aws_instance" "web" {
-  # ami           = data.aws_ami.app_ami.id
-  # instance_type = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 # Creates four identical aws ec2 instances
   count = 4     
-  
-  # All four instances will have the same ami and instance_type
-  ami = lookup(var.ec2_ami,var.region) 
-  instance_type = var.instance_type # 
+
+
   tags = {
-    # The count.index allows you to launch a resource 
-    # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "web-${count.index}"
-  }
+   Name = "ACME Company Terraform Presentation"
+   }
 }
- 
-  # tags = {
-  #   Name = "ACME Company Terraform Presentation"
-  # }
- #}
