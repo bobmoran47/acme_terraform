86f6aa02e877102fe43bc6a75cace9273fc9f36b
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-12 18:27:10 -0400

    Autoscaling Dev Env - Updating Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-#output "instance_ami" {
-  #value = aws_instance.acme.ami
-#}
-
-#output "instance_arn" {
-  #value = aws_instance.acme.arn
-#}
+output "environment_url" {
+  value = module.acme_alb.lb_dns_name
+}
